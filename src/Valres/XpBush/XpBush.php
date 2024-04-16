c30838220b0ab1e8ffaf6bd2cc354c65c723b160
ValresMC <tsterle59@gmail.com>
2024-04-16 17:21:52 +0200

    .

---
+++ src/Valres/XpBush/XpBush.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Valres\XpBush;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+
+class XpBush extends PluginBase
+{
+    use SingletonTrait;
+    protected function onEnable(): void {
+
+    }
+
+    protected function onLoad(): void {
+
+    }
+}
